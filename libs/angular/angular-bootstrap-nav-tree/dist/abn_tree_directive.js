fc3cdb2b02b81bf6f3b03ba29be14a8fcdf814ce
hinet <63603636@qq.com>
2017-03-07 10:34:55 +0800

    同步最新

--- libs/angular/angular-bootstrap-nav-tree/dist/abn_tree_directive.js
+++ libs/angular/angular-bootstrap-nav-tree/dist/abn_tree_directive.js
@@ -140,6 +140,7 @@
                 return b.uid = "" + Math.random();
               }
             });
+            console.log('UIDs are set.');
             for_each_branch(function(b) {
               var child, _i, _len, _ref, _results;
               if (angular.isArray(b.children)) {
@@ -240,6 +241,7 @@
             });
           }
           n = scope.treeData.length;
+          console.log('num root branches = ' + n);
           for_each_branch(function(b, level) {
             b.level = level;
             return b.expanded = b.level < expand_level;
