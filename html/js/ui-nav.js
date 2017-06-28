5caba1727a09230c64d32c1e3c89c64eb2cbde3e
hinet <63603636@qq.com>
2017-06-28 10:26:53 +0800

    选中菜单防刷新

--- html/js/ui-nav.js
+++ html/js/ui-nav.js
@@ -1,7 +1,22 @@
 +function ($) {
 
   $(function(){
-
+      //默认激活选项
+      var hash = window.location.href;
+      if(typeof(hash.match(/http:\/\/(.*?)\/(.*)/)[2]) != 'undefined'){
+          //匹配后缀有数字的链接(如：编辑/删除 /admin/news/edit/1)，返回/admin/news
+          var operation = hash.match(/http:\/\/(.*?)\/(.*)\/(.*)\/(\d+)$/);
+          if(typeof(operation) != 'undefined' && operation){
+              $url = operation[2];
+          }else{
+              $url = hash.match(/http:\/\/(.*?)\/(.*)/)[2];
+          }
+          var $this = $("[ui-nav] a[href$='"+$url+"']");
+          $this.is('a') || ($this = $this.closest('a'));
+          $this.parents('ul').parent().toggleClass('active');
+          $this.parent().addClass('active');
+          $this.next().is('ul') && e.preventDefault();
+      }
       // nav
       $(document).on('click', '[ui-nav] a', function (e) {
         var $this = $(e.target), $active;
